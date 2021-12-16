7433bea679a8747061e2a8d2b15a7ca8ad35f083
idk <dkp11102002@gmail.com>
2021-12-16 17:50:29 +0530

    Data Types and Other basic stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+        int a,b,c;
+        a=1; b=2; c=3;
+        int sum = a+b+c;
+        System.out.print(sum);
+
     }
 }
