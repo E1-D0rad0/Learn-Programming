bc557726d761bac7d08bda8a1df0df0efa0f12da
idk <dkp11102002@gmail.com>
2021-12-16 17:52:44 +0530

    Data Types and other basic stuff

---
+++ src/com/company/literals.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class literals {
+    public static void main(String[] args){
+        byte age = 126;
+        short s1 = 1344;
+        int i1 = 133214214;
+        long l1 = 342343243242L;
+        char ch = 'A';
+        float f1 = 5.6F;
+        double d1 = 4.65;
+        boolean a = true;
+        System.out.println(f1);
+        String str = "Men";
+        System.out.println(str);
+    }
+}
